a836e74c4d15fbc40526b1081ffd416b2a8373e4
Alain Linden <alain@lindenlab.com>
2011-02-22 09:16:49 -0800

    build working on windows.

--- build-cmd.sh
+++ build-cmd.sh
@@ -17,6 +17,8 @@ fi
 
 OGG_VERSION=1.1.3
 OGG_SOURCE_DIR="libogg-$OGG_VERSION"
+VORBIS_VERSION=1.2.0
+VORBIS_SOURCE_DIR=libvorbis-$VORBIS_VERSION
 
 # load autbuild provided shell functions and variables
 eval "$("$AUTOBUILD" source_environment)"
@@ -24,48 +26,44 @@ eval "$("$AUTOBUILD" source_environment)"
 top="$(pwd)"
 stage="$(pwd)/stage"
 
+case "$AUTOBUILD_PLATFORM" in
+    "windows")
+        pushd "$OGG_SOURCE_DIR"
+
+        packages="$(cygpath -m "$stage/packages")"
+
+        build_sln "win32/ogg.sln" "Debug|Win32" "ogg_static"
+        build_sln "win32/ogg.sln" "Release|Win32" "ogg_static"
+
+        mkdir -p "$stage/lib"/{debug,release}
+        cp "win32/Static_Debug/ogg_static_d.lib" "$stage/lib/debug/ogg_static_d.lib"
+        cp "win32/Static_Debug/vc100.pdb" "$stage/lib/debug/ogg_static_d.pdb"
+        cp "win32/Static_Release/ogg_static.lib" "$stage/lib/release/ogg_static.lib"
+        cp "win32/Static_Release/vc100.pdb" "$stage/lib/release/ogg_static.pdb"
+
+        mkdir -p "$stage/include"
+        cp -a "include/ogg/" "$stage/include/"
+        
+        popd
+        pushd "$VORBIS_SOURCE_DIR"
+        
+        build_sln "win32/vorbis.sln" "Debug|Win32" "vorbis_static"
+        build_sln "win32/vorbis.sln" "Release|Win32" "vorbis_static"
+        
+        cp "win32/Vorbis_Static_Debug/vorbis_static_d.lib" "$stage/lib/debug/vorbis_static.lib"
+        cp "win32/Vorbis_Static_Debug/vc100.pdb" "$stage/lib/debug/vorbis_static.pdb"
+        cp "win32/Vorbis_Static_Release/vorbis_static.lib" "$stage/lib/release/vorbis_static.lib"
+        cp "win32/Vorbis_Static_Release/vc100.pdb" "$stage/lib/release/vorbis_static.pdb"
+        cp -a "include/vorbis/" "$stage/include/"
+        popd
+    ;;
+    "darwin")
+    ;;
+    "linux")
+    ;;
+esac
+mkdir -p "$stage/LICENSES"
 pushd "$OGG_SOURCE_DIR"
-    case "$AUTOBUILD_PLATFORM" in
-        "windows")
-            packages="$(cygpath -m "$stage/packages")"
-            load_vsvars
-
-			#devenv.com /Upgrade "win32/ogg.dsw"
-
-			build_sln "win32/ogg.dsw" "Debug|Win32"
-            build_sln "win32/ogg.dsw" "Release|Win32"
-
-            mkdir -p "$stage/lib"/{debug,release}
-            cp "win32/Static_Debug/ogg_static_d.lib" "$stage/lib/debug/ogg_static_d.lib"
-            cp "win32/Static_Debug/vc100.pdb" "$stage/lib/debug/ogg_static_d.pdb"
-            cp "win32/Static_Release/ogg_static.lib" "$stage/lib/release/ogg_static.lib"
-            cp "win32/Static_Release/vc100.pdb" "$stage/lib/release/ogg_static.pdb"
-
-            mkdir -p "$stage/include"
-            cp -a "include/ogg/" "$stage/include/"
-        ;;
-        "darwin")
-            # TODO: this produces a package that actually will create link errors when building the viewer. Notes:
-            # - Try to compile and link against our own OpenSSL (right now though, openssl-autobuild does not build for Mac)
-            # - May be same thing for zlib
-            # - Disabling ldap suppresses half of the link errors so that's something to keep
-            opts='-arch i386 -iwithsysroot /Developer/SDKs/MacOSX10.4u.sdk'
-            CFLAGS="$opts" CXXFLAGS="$opts" ./configure  --disable-ldap --disable-ldaps --with-ssl --prefix="$stage"
-            make
-            make install
-            mkdir -p "$stage/lib/release"
-            cp "$stage/lib/libogg.a" "$stage/lib/release"
-        ;;
-        "linux")
-            # TODO: see darwin notes here above
-            CFLAGS=-m32 CXXFLAGS=-m32 ./configure --disable-ldap --disable-ldaps --with-ssl --prefix="$stage"
-            make
-            make install
-            mkdir -p "$stage/lib/release"
-            cp "$stage/lib/libogg.a" "$stage/lib/release"
-        ;;
-    esac
-    mkdir -p "$stage/LICENSES"
     cp COPYING "$stage/LICENSES/ogg-vorbis.txt"
 popd
 
