b9b46fdd05bf29134710e0c0a06da6429f6bfd8c
callum_linden <none@none>
2016-02-29 15:22:31 -0800

    DRTVRW-418 Initial set of changes for Windows/Windows64 builds

--- build-cmd.sh
+++ build-cmd.sh
@@ -26,22 +26,18 @@ eval "$("$AUTOBUILD" source_environment)"
 top="$(pwd)"
 stage="$(pwd)/stage"
 
-
 build=${AUTOBUILD_BUILD_ID:=0}
 echo "${OGG_VERSION}-${VORBIS_VERSION}.${build}" > "${stage}/VERSION.txt"
 
 case "$AUTOBUILD_PLATFORM" in
-    "windows")
+    windows*)
         pushd "$OGG_SOURCE_DIR"
 
         packages="$(cygpath -m "$stage/packages")"
 
-        build_sln "win32/ogg.sln" "Debug|Win32" "ogg_static"
-        build_sln "win32/ogg.sln" "Release|Win32" "ogg_static"
+        build_sln "win32/ogg.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM" "ogg_static"
 
-        mkdir -p "$stage/lib"/{debug,release}
-        cp "win32/Static_Debug/ogg_static_d.lib" "$stage/lib/debug/ogg_static_d.lib"
-        cp "win32/Static_Debug/vc120.pdb" "$stage/lib/debug/ogg_static_d.pdb"
+        mkdir -p "$stage/lib/release"
         cp "win32/Static_Release/ogg_static.lib" "$stage/lib/release/ogg_static.lib"
         cp "win32/Static_Release/vc120.pdb" "$stage/lib/release/ogg_static.pdb"
 
@@ -51,23 +47,14 @@ case "$AUTOBUILD_PLATFORM" in
         popd
         pushd "$VORBIS_SOURCE_DIR"
         
-        build_sln "win32/vorbis.sln" "Debug|Win32" "vorbis_static"
-        build_sln "win32/vorbis.sln" "Release|Win32" "vorbis_static"
-        build_sln "win32/vorbis.sln" "Debug|Win32" "vorbisenc_static"
-        build_sln "win32/vorbis.sln" "Release|Win32" "vorbisenc_static"
-        build_sln "win32/vorbis.sln" "Debug|Win32" "vorbisfile_static"
-        build_sln "win32/vorbis.sln" "Release|Win32" "vorbisfile_static"
+        build_sln "win32/vorbis.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM" "vorbis_static"
+        build_sln "win32/vorbis.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM" "vorbisenc_static"
+        build_sln "win32/vorbis.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM" "vorbisfile_static"
         
-        cp "win32/Vorbis_Static_Debug/vorbis_static_d.lib" "$stage/lib/debug/vorbis_static_d.lib"
-        cp "win32/Vorbis_Static_Debug/vc120.pdb" "$stage/lib/debug/vorbis_static_d.pdb"
         cp "win32/Vorbis_Static_Release/vorbis_static.lib" "$stage/lib/release/vorbis_static.lib"
         cp "win32/Vorbis_Static_Release/vc120.pdb" "$stage/lib/release/vorbis_static.pdb"
-        cp "win32/VorbisEnc_Static_Debug/vorbisenc_static_d.lib" "$stage/lib/debug/vorbisenc_static_d.lib"
-        cp "win32/VorbisEnc_Static_Debug/vc120.pdb" "$stage/lib/debug/vorbisenc_static_d.pdb"
         cp "win32/VorbisEnc_Static_Release/vorbisenc_static.lib" "$stage/lib/release/vorbisenc_static.lib"
         cp "win32/VorbisEnc_Static_Release/vc120.pdb" "$stage/lib/release/vorbis_static.pdb"
-        cp "win32/VorbisFile_Static_Debug/vorbisfile_static_d.lib" "$stage/lib/debug/vorbisfile_static_d.lib"
-        cp "win32/VorbisFile_Static_Debug/vc120.pdb" "$stage/lib/debug/vorbis_static_d.pdb"
         cp "win32/VorbisFile_Static_Release/vorbisfile_static.lib" "$stage/lib/release/vorbisfile_static.lib"
         cp "win32/VorbisFile_Static_Release/vc120.pdb" "$stage/lib/release/vorbis_static.pdb"
         cp -a "include/vorbis/" "$stage/include/"
