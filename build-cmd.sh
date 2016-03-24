66134f1f5ca04fbab2f6796d8bc728bd62ce2795
Nat Goodspeed <nat@lindenlab.com>
2016-03-24 16:20:25 -0400

    DRTVWR-418: Fix regexps used to extract ogg and vorbis versions.

--- build-cmd.sh
+++ build-cmd.sh
@@ -20,10 +20,10 @@ else
 fi
 
 OGG_SOURCE_DIR="libogg"
-OGG_VERSION="$(sed -n 's/^ VERSION=(.*)$/\1/p' "$OGG_SOURCE_DIR/configure")"
+OGG_VERSION="$(sed -n 's/^ VERSION=\(.*\)$/\1/p' "$OGG_SOURCE_DIR/configure")"
 
 VORBIS_SOURCE_DIR="libvorbis"
-VORBIS_VERSION="$(sed -n "s/^PACKAGE_VERSION='(.*)'/\1/p" "$VORBIS_SOURCE_DIR/configure")"
+VORBIS_VERSION="$(sed -n "s/^PACKAGE_VERSION='\(.*\)'/\1/p" "$VORBIS_SOURCE_DIR/configure")"
 
 # load autbuild provided shell functions and variables
 eval "$("$autobuild" source_environment)"
